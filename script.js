f035fc29fbe41e2d684d7b686f52c698e43625d9
Steffen Zierold <steffen.zierold@outlook.com>
2023-11-11 19:40:42 +0100

    added overlays to tiles and made them functional

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-const hiddenOverlayOne = document.getElementById("pOne-overlay");
-const BlaBlub = document.getElementById("pOne");
-const hiddenOverlayTwo = document.getElementById("pTwo-overlay");
-const hiddenOverlayThree = document.getElementById("pThree-overlay");
+const overlayOne = document.getElementById("pOne-overlay");
+const pOneTile = document.getElementById("pOne");
+const overlayTwo = document.getElementById("pTwo-overlay");
+const pTwoTile = document.getElementById("pTwo");
+const overlayThree = document.getElementById("pThree-overlay");
+const pThreeTile = document.getElementById("pThree");
 
 const showOverlayOne = () => {
-  hiddenOverlayOne.style.display = "flex";
+  overlayOne.style.display = "flex";
+};
+const hideOverlayOne = () => {
+  overlayOne.style.display = "none";
 };
 
-BlaBlub.addEventListener("mouseover", showOverlayOne);
+const showOverlayTwo = () => {
+  overlayTwo.style.display = "flex";
+};
+const hideOverlayTwo = () => {
+  overlayTwo.style.display = "none";
+};
+
+const showOverlayThree = () => {
+  overlayThree.style.display = "flex";
+};
+const hideOverlayThree = () => {
+  overlayThree.style.display = "none";
+};
+
+pOneTile.addEventListener("mouseover", showOverlayOne);
+pTwoTile.addEventListener("mouseover", showOverlayTwo);
+pThreeTile.addEventListener("mouseover", showOverlayThree);
+
+pOneTile.addEventListener("mouseout", hideOverlayOne);
+pTwoTile.addEventListener("mouseout", hideOverlayTwo);
+pThreeTile.addEventListener("mouseout", hideOverlayThree);
